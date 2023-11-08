328bcfd862c8fc6c704efbd38edbb67a22109353
marcel <marcelharder@protonmail.com>
2023-11-08 12:29:15 +0100

    nog steeds bezig met de report

--- Controllers/SuggestionController.cs
+++ Controllers/SuggestionController.cs
@@ -12,7 +12,7 @@ public class SuggestionController : ControllerBase
     private IPreviewReport _previewReport;
     private IProcedureRepository _proc;
 
-   
+
     public SuggestionController(ISuggestion repo, IPreviewReport previewReport, IProcedureRepository proc, IMapper map)
     {
         _repo = repo;
@@ -29,10 +29,9 @@ public class SuggestionController : ControllerBase
     }
 
     [HttpGet("{id}/{soort}", Name = "GetSuggestion")] // gets recorded suggestion for this user by the soort
-    public async Task<IActionResult> GetA(string id ,int soort)
+    public async Task<IActionResult> GetA(string id, int soort)
     {
-        var p = await _repo.GetIndividualSuggestion(soort,id);
-
+        var p = await _repo.GetIndividualSuggestion(soort, id);
         return Ok(p);
     }
 
@@ -42,21 +41,21 @@ public class SuggestionController : ControllerBase
         // Save the preview report first
         int pvr_result = await _previewReport.updatePVR(cp);
 
-        
+
         var currentProcedure = await _proc.getSpecificProcedure(cp.procedure_id);
         // get the currentuser from cp
         var currentUser = currentProcedure.SelectedSurgeon;
         // get the soort from fdType from cp
         var soort = currentProcedure.fdType;
         // get the current suggestion, if not available a new one is generated for this user and soort                     
-        var current_suggestion = await _repo.GetIndividualSuggestion(soort,currentUser.ToString());
+        var current_suggestion = await _repo.GetIndividualSuggestion(soort, currentUser.ToString());
 
-       // Class_Suggestion c = await _repo.mapToSuggestionFromPreview(current_suggestion, cp);
+        // Class_Suggestion c = await _repo.mapToSuggestionFromPreview(current_suggestion, cp);
 
         Class_Suggestion c = _map.Map<Class_Preview_Operative_report, Class_Suggestion>(cp, current_suggestion);
         c.soort = soort;
         c.user = currentUser.ToString();
-        
+
 
         var result = await _repo.updateSuggestion(c);
         return Ok(result);
@@ -65,7 +64,12 @@ public class SuggestionController : ControllerBase
     [HttpPost]
     public async Task<IActionResult> Post(Class_Suggestion c)
     {
-        var p = await _repo.AddIndividualSuggestion(c);
+        var p = new Class_Suggestion();
+        if (await _repo.GetIndividualSuggestion(c.soort, c.user) == null)
+        {
+            p = await _repo.AddIndividualSuggestion(c);
+        }
+
         return Ok(p);
     }
 
